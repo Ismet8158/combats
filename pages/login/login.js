c345d8e8419daf835e3528e55d3fa658cddb1500
XimikRU <ermachkov-ilya1999@mail.ru>
2018-05-17 19:32:47 +0300

    remade to promises, minor bug fixes

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,30 +1,71 @@
- window.addEventListener("load", () => {
-     var old = getUser();
-     if (old) {
-         apiRequest(`/whoami?user_id=${old.token}`)
-             .then(text => {
-                 res = JSON.parse(text);
-                 if (res.status === 'ok') {
-                     window.location = "/ready/";
-                 }
-             });
-     }
+function whoAmI() {
+    return new Promise((resolve, reject) => {
+        var localUser = getUser();
+        // typeof null -----> "object"
+        if (localUser.token != null && localUser.username != null) {
+            apiRequest(`/whoami?user_id=${localUser.token}`)
+                .then(apiAnswer => {
+                    parsedAnswer = JSON.parse(apiAnswer);
+                    if (parsedAnswer.status === 'ok') {
+                        console.log('whoami done resolving ...');
+                        resolve(parsedAnswer);
+                    } else {
+                        reject('status is not ok rejecting ...');
+                    }
+                })  
+                .catch(reason => {
+                    console.log('Caught error in whoaimi: ' + reason);
+                });
+        }
+    });
+}
 
-     document.querySelector('.btn-login').onclick = function () {
-         var username = document.querySelector('.inp-username').value;
-         apiRequest('/register', {
-                 method: 'POST',
-                 body: `username=${username}`
-             })
-             .then(text => {
-                 var res = JSON.parse(text);
-                 if (res.status === 'ok') {
-                     setUserId({
-                         username: res.user.username,
-                         token: res.user.id
-                     });
-                     window.location = "/ready/";
-                 }
-             });
-     }
- })
+function registerUser() {
+    return new Promise((resolve, reject) => {
+        var username = document.querySelector('.inp-username').value;
+        apiRequest('/register', {
+                method: 'POST',
+                body: `username=${username}`
+            })
+            .then(apiAnswer => {
+                var parsedAnswer = JSON.parse(apiAnswer);
+                if (parsedAnswer.status === 'ok') {
+                    // TODO1: проверку на успешность фукнции SetUserId;
+                    setUserId({
+                        username: parsedAnswer.user.username,
+                        token: parsedAnswer.user.id
+                    })
+                    whoAmI().then(result => {
+                        console.log('register done resolving ...');
+                        resolve(result);
+                    })
+                    // TODO1: Иначе reject;
+                }
+            })
+            .catch(reason => {
+                console.log('Caught error in register: ' + reason);
+            })
+    });
+}
+
+window.addEventListener("load", function() {
+    whoAmI()
+        .then(result => {
+            alert('load -> reditect');
+            window.location = "/ready/";
+        })
+        .catch(reason => {
+            console.log('onload - rejected on whoaim: ' + reason);
+        })
+});
+
+document.querySelector('.btn-login').onclick = function() {
+    registerUser()
+        .then(result => {
+            alert('button -> redirect');
+            window.location = "/ready/";
+        })
+        .catch(reason => {
+            console.log('onclick - rejected on register: ' + reason);
+        });
+};
