7cd11ade4d12961ac57a1b52096d27dfe8b72326
XimikRU <ermachkov-ilya1999@mail.ru>
2018-05-19 16:40:16 +0300

    moved whoAmI() from login.js into common.js

--- pages/common/common.js
+++ pages/common/common.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
     function apiRequest(request, {
         method,
         body
@@ -19,28 +19,49 @@
 
     function getUser() {
         return {
-            username: localStorage.getItem("username"),
-            token: localStorage.getItem("usertoken")
+            username: localStorage.getItem('username'),
+            token: localStorage.getItem('usertoken')
         };
     }
 
     function setCombatObject(combat) {
         localStorage.setItem('combat', JSON.stringify(combat));
-        //localStorage.setItem('combat_id', combat.combat_id);
     }
 
     function getCombatObject() {
-        //return combatObj = localStorage.getItem('combat_id');
         var combatObj = localStorage.getItem('combat');
         if (combatObj)
             return JSON.parse(JSON.parse(combatObj));
         else
             return null;
-
     }
 
     function removeUserId() {
         localStorage.removeItem('user');
+    }
+
+    function whoAmI() {
+        return new Promise((resolve, reject) => {
+            var localUser = getUser();
+            if (localUser.token !== null && localUser.username !== null) {
+                apiRequest(`/whoami?user_id=${localUser.token}`)
+                    .then(apiAnswer => {
+                        parsedAnswer = JSON.parse(apiAnswer);
+                        if (parsedAnswer.status === 'ok') {
+                            console.log('whoami done resolving ...');
+                            resolve(parsedAnswer);
+                        } else {
+                            reject('API response status is not ok rejecting ...');
+                        }
+                    })  
+                    .catch(reason => {
+                        reject('Caught error in whoaimi APIrequest: ' + reason);
+                    });
+            }
+            else{
+                reject('cant get local user rejecting ...');
+            }
+        });
     }
 
     window.apiRequest = apiRequest;
@@ -48,4 +69,5 @@
     window.getUser = getUser;
     window.setCombatObject = setCombatObject;
     window.getCombatObject = getCombatObject;
+    window.whoAmI = whoAmI;
 })();
