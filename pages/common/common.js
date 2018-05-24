536f951bf42961f329c91f4260b75f779ec20326
XimikRU <ermachkov-ilya1999@mail.ru>
2018-05-24 18:18:19 +0300

    UPD: setUserId; FIX: get/set CombatObject

--- pages/common/common.js
+++ pages/common/common.js
@@ -15,6 +15,12 @@
     function setUserId(val) {
         localStorage.setItem('username', val.username);
         localStorage.setItem('usertoken', val.token);
+        
+        var localUser = getUser();
+        if (localUser.username === val.username && localUser.token === val.token)
+            return true;
+        else
+            return false;
     }
 
     function getUser() {
@@ -25,13 +31,14 @@
     }
 
     function setCombatObject(combat) {
-        localStorage.setItem('combat', JSON.stringify(combat));
+        localStorage.setItem('combat', combat);
+        return true;
     }
 
     function getCombatObject() {
         var combatObj = localStorage.getItem('combat');
         if (combatObj)
-            return JSON.parse(JSON.parse(combatObj));
+            return JSON.parse(combatObj);
         else
             return null;
     }
@@ -40,6 +47,7 @@
         localStorage.removeItem('user');
     }
 
+    //TODO: Убрать лишние промисы.
     function whoAmI() {
         return new Promise((resolve, reject) => {
             var localUser = getUser();
